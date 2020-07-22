c543dfdf18cf3cd48fe536cfb8c6d2497a48adba
nv73 <47676846+nv73@users.noreply.github.com>
2020-07-21 18:55:49 -0700

    Update - 2020-07-21
    AdaBoost and Random Forest for hyperspectral imagery now functioning!

--- tilePicker.py
+++ tilePicker.py
@@ -4,6 +4,15 @@ Created on Tue Jul  7 17:14:27 2020
 
 @author: nick.viner
 """
+
+"""
+Side project in an attempt to allow easier access to machine learning methods for 
+the classification of geo-imagery with limited sample sizes.
+"""
+
+"""
+Currently focused on pixel based classification of hyperspectral imagery.
+"""
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PIL import Image, ImageQt
 from sys import argv
@@ -12,10 +21,13 @@ import gdal
 import numpy as np
 import osr
 from os.path import basename
+import pickle
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics, preprocessing
+import pandas as pd
 
 class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
-    
-    tileSizeSignal = QtCore.pyqtSignal(int)
     
     def __init__(self, parent = None):
         
@@ -28,7 +40,7 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         self.canvasView.addWidget(self.canvas)
         
         #Set up the layout for interacting with loaded image files
-        self.loadedImagesTable = tableWidget()
+        self.loadedImagesTable = tableWidget(deleteEnabled=False)
         self.loadedImagesTableLayout.addWidget(self.loadedImagesTable)
         self.loadedImagesTable.setColumnCount(1)
         self.loadedImagesTable.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
@@ -38,6 +50,7 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         self.selectedTilesTableLayout.addWidget(self.selectedTilesTable)
         self.selectedTilesTable.setColumnCount(1)
         self.selectedTilesTable.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.selectedTilesTable.setHorizontalHeaderLabels(["Index, X, Y, Class, Filename, Sample Datapoint"])
         
         # Containers for GUI variables
         self.tileSize = self.tileSizeEdit.text()
@@ -49,8 +62,17 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         self.tileSelectRadio.toggled.connect(self.tileSelectRadioUpdate)
         self.spectrumSelectRadio.toggled.connect(self.spectrumSelectRadioUpdate)
         self.bandSelectCombo.currentTextChanged.connect(self.updateActiveBand)
+        self.bandSelectCombo.currentTextChanged.connect(self.canvas.displayGeoImage)
         self.classNameCombo.currentTextChanged.connect(self.updateActiveClass)
         self.canvas.tileAdded.connect(self.updateSelectedTiles)
+        self.selectedTilesTable.cellDeleted.connect(self.updateTileIndexOnDelete)
+        self.actionAdd_New_Object_Type.triggered.connect(self.addNewObjectClass)
+        self.actionExport_Objects_to_File.triggered.connect(self.canvas.exportTilesToFile)
+        self.actionAdaBoostTrain.triggered.connect(self.canvas.trainAdaBoost)
+        self.actionRfTrain.triggered.connect(self.canvas.trainRandomForest)
+        
+        #Test button
+        self.pushButton.clicked.connect(self.canvas.classify_all)
         
         #Add default object classes
         defaultClasses = ["Tree", "Snow", "Water", "Asphalt", "Soil", "Rock"]
@@ -58,13 +80,31 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         for i in defaultClasses:
             
             self.classNameCombo.addItem(i)
+            
+    def addNewObjectClass(self):
+        
+        newClass = QtWidgets.QInputDialog.getText(self, "Input New Class", "Input New Class")
+
+        self.classNameCombo.addItem(newClass[0])
+    
+    def updateTileIndexOnDelete(self):
+        
+        valueToRemove = self.selectedTilesTable.lastDeletedCell.split(",")[0]
+        
+        self.canvas.tiles[int(valueToRemove)].cleared = True
     
     def updateSelectedTiles(self):
         
-        tileInfo = "%i, %i, %s, %s" % (self.canvas.tiles[self.canvas.tile_index].x, 
+        x = self.canvas.tiles[self.canvas.tile_index].x
+        y = self.canvas.tiles[self.canvas.tile_index].y
+        z = int(self.canvas.activeBand) - 1
+        
+        tileInfo = "%i, %i, %i, %s, %s, %i" % (self.canvas.tile_index, self.canvas.tiles[self.canvas.tile_index].x, 
                                    self.canvas.tiles[self.canvas.tile_index].y,
                                    self.canvas.tiles[self.canvas.tile_index].classification,
-                                   basename(self.canvas.activeGeoImagePath))
+                                   basename(self.canvas.activeGeoImagePath),
+                                   self.canvas.activeGeoArray[z, y, x]
+                                   )
         
         self.selectedTilesTable.addToNext(tileInfo)
         
@@ -73,8 +113,8 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         #Prevent user from using multiple selection types when choosing tiles
         if len(self.canvas.tiles) > 0:
             
-            pass
-        
+            self.spectrumSelectRadio.setEnabled(False)
+            
         else:
         
             self.tileSizeEdit.setReadOnly(False)
@@ -88,8 +128,8 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         #Prevent user from using multiple selection types when choosing tiles
         if len(self.canvas.tiles) > 0:
             
-            pass
-        
+            self.tileSelectRadio.setEnabled(False)
+            
         else:
         
             self.tileSizeEdit.setReadOnly(True)
@@ -168,13 +208,17 @@ class tilePicker_Form(QtWidgets.QMainWindow, tilePicker_ui.Ui_imageTilePicker):
         
         self.bandSelectCombo.clear()
         
+        self.canvas.total_bands = bands
+        
         for f in range(1, bands + 1):
             
             self.bandSelectCombo.addItem(str(f))
 
 class tableWidget(QtWidgets.QTableWidget):
     
-    def __init__(self):
+    cellDeleted = QtCore.pyqtSignal()
+    
+    def __init__(self, deleteEnabled = True):
         
         super(tableWidget, self).__init__()
         
@@ -182,17 +226,23 @@ class tableWidget(QtWidgets.QTableWidget):
         
         self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         
-        self.itemDoubleClicked.connect(self.deleteCell)
+        if deleteEnabled == True:
+            
+            self.itemDoubleClicked.connect(self.deleteCell)
         
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         
-        
+        self.lastDeletedCell = None
         
     def deleteCell(self):
         
         cleared = QtWidgets.QTableWidgetItem("Cleared")
-        print(self.currentItem().text())
+        
+        self.lastDeletedCell = self.currentItem().text()
+        
         self.setItem(self.currentItem().row(), self.currentItem().column(), cleared)
+        
+        self.cellDeleted.emit()
         
     def addToNext(self, inVal, column = 0):
         
@@ -232,7 +282,7 @@ class geoCanvas(QtWidgets.QGraphicsView):
     
     tileAdded = QtCore.pyqtSignal()
     
-    patchSizeChanged = QtCore.pyqtSignal(int)
+    spectrumAdded = QtCore.pyqtSignal()
     
     def __init__(self):
 
@@ -265,16 +315,18 @@ class geoCanvas(QtWidgets.QGraphicsView):
         self.activeTileShape = QtWidgets.QGraphicsRectItem(0,0, self.patchSize, self.patchSize)
         self.tiles = {}
         self.tile_index = 0
+        self.total_bands = 0
 
         # Coordinate values for the mouse cursor
         self.mouse_coordinates = None
         self.selected_coordinates = None
 
         # Graphical coordinate indicators
+        self.pointSize = 20
         self.displayed_coordinates = QtWidgets.QGraphicsTextItem()
         self.displayed_coordinates.setTransformOriginPoint(self.displayed_coordinates.boundingRect().topLeft())
         self.displayed_coordinates_font = self.displayed_coordinates.font()
-        self.displayed_coordinates_font.setPointSize(20)
+        self.displayed_coordinates_font.setPointSize(self.pointSize)
         self.displayed_coordinates.setFont(self.displayed_coordinates_font)
         self.displayed_coordinates_scale = 1
 
@@ -285,9 +337,14 @@ class geoCanvas(QtWidgets.QGraphicsView):
         #Active geo image information
         self.activeGeoImagePath = None
         self.activeGeoArray = None
-        self.activeBand = None
-        
+        self.activeBand = 0
         self.activeClass = "Tree"
+        
+        #Machine Learning stuff
+        self.labels = None
+        self.testFeatures = None
+        self.adaBoostModel = None
+        self.randomForestModel = None
 
         
     # Taken from https://stackoverflow.com/questions/35508711/how-to-enable-pan-and-zoom-in-a-qgraphicsvie
@@ -412,17 +469,13 @@ class geoCanvas(QtWidgets.QGraphicsView):
                 
                 self.scene.addItem(rect)
                 
-               
-                if self.isSpectrum == False:
-                    
-                    dataType = "tile"
-                    
-                else:
-                    
-                    dataType = "spectrum"
-                    
-                newTile = tile(self.activeGeoArray[tileCorner[1]:tileCorner[1] + self.patchSize, tileCorner[0]:tileCorner[0] + self.patchSize],
-                               tileCorner[0], tileCorner[1], self.activeClass, dataType)
+                #Testing print statements
+                #print(self.activeGeoArray.shape)
+                #print(self.activeGeoArray[:,tileCorner[0]: tileCorner[0] + 10, tileCorner[1]: tileCorner[1] + 10])
+                #print(self.patchSize)
+                
+                newTile = tile(self.activeGeoArray[:,tileCorner[1]:tileCorner[1] + self.patchSize, tileCorner[0]:tileCorner[0] + self.patchSize],
+                               tileCorner[0], tileCorner[1], self.activeClass, self.patchSize)
  
                 
                 self.tiles[self.tile_index] = newTile
@@ -431,15 +484,20 @@ class geoCanvas(QtWidgets.QGraphicsView):
                 
             else:
                 
-                xy = (selected_coordinates.x(), selected_coordinates.y())
+                x = int(selected_coordinates.x())
+                y = int(selected_coordinates.y())
                 
-                lineH = QtWidgets.QGraphicsLineItem(xy[0] - 10, xy[1], xy[0] + 10, xy[1])
-                lineV = QtWidgets.QGraphicsLineItem(xy[0], xy[1] + 10, xy[0], xy[1] - 10)
+                lineH = QtWidgets.QGraphicsLineItem(x - 5, y, x + 5, y)
+                lineV = QtWidgets.QGraphicsLineItem(x, y + 5, x, y - 5)
                 
                 self.scene.addItem(lineH)
                 self.scene.addItem(lineV)
+
+                newSpectrum = spectrum(self.activeGeoArray[:, y, x], x, y, self.activeClass, len(self.activeGeoArray[:,y,x]))
+
+                self.tiles[self.tile_index] = newSpectrum
                 
-                arr = activeGeoArray[:,xy[1], xy[0]]
+                self.tileAdded.emit()
             
             self.tile_index += 1
             
@@ -450,6 +508,26 @@ class geoCanvas(QtWidgets.QGraphicsView):
         super(geoCanvas, self).mouseReleaseEvent(event)
 
         self.viewport().setCursor(QtCore.Qt.CrossCursor)
+        
+    def keyPressEvent(self, event):
+        
+        if event.key() == 61:
+            
+            self.displayed_coordinates_scale = self.displayed_coordinates_scale * 0.8
+            self.displayed_coordinates.setScale(self.displayed_coordinates_scale)
+            
+        if event.key() == 45:
+            
+            if self.pointSize == 1:
+                
+                pass
+            
+            else:
+                
+                self.displayed_coordinates_scale = self.displayed_coordinates_scale * 1.25
+                self.displayed_coordinates.setScale(self.displayed_coordinates_scale)
+        
+        super(geoCanvas, self).keyPressEvent(event)
 
     def mouseMoveEvent(self, event):
 
@@ -493,7 +571,7 @@ class geoCanvas(QtWidgets.QGraphicsView):
             
         except Exception as e:
             
-            pass
+            print(e)
         
         if len(arr.shape) != 2:
 
@@ -537,11 +615,11 @@ class geoCanvas(QtWidgets.QGraphicsView):
         
         self.activeGeoArray = self.readAs3DArray(geoImagePath)
         
-        self.displayGeoImage()
+        self.displayGeoImage(band=self.activeBand)
     
     def displayGeoImage(self, band=0):
 
-        self.displayArray(self.activeGeoArray[band])
+        self.displayArray(self.activeGeoArray[int(band)])
         
     def getGeodetics(self, inputGeoImage):
         
@@ -569,6 +647,139 @@ class geoCanvas(QtWidgets.QGraphicsView):
             arr[band] = gimage.GetRasterBand(band + 1).ReadAsArray(0,0,x,y)
             
         return(arr)
+    
+    def exportTilesToFile(self):
+        
+        fname = QtWidgets.QFileDialog.getSaveFileName(self, "Save Tiles","",".til(*.til)")
+        
+        pickle.dump(self.tiles, open(fname[1], 'wb'))
+        
+    def tilesToDataframe(self):
+        
+        data = self.tiles
+        
+        spectrumHeader = [str(x) for x in range(self.total_bands)]
+        spectrumHeader.append("class")
+        
+        spectrumDf = pd.DataFrame(columns=spectrumHeader)
+        spectrumDf["id"] = [x for x in range(len(data))]
+        
+        #Band iterator
+        b = 0
+        
+        #For each spectrum object
+        for i in data:
+            
+            #Name of classification
+            c = data[i].classification
+            
+            #Array containing an intensity value for each band
+            spec = data[i].data
+            
+            #for each band in the spectrum
+            for band in range(len(spec)):
+                
+                #assign the b'th value of the associated band column to it's respective
+                #intensity value
+                spectrumDf[str(band)][b] = spec[band]
+                spectrumDf["class"][b] = c 
+                
+            #increase the iterator
+            b+=1
+
+        return(spectrumDf)
+    
+    def encode_labels(self, df, column):
+        
+        uniqueLabels = df[column].unique()
+        
+        encoded_labels = {}
+        
+        i = 0
+        
+        for label in uniqueLabels:
+            
+            encoded_labels[label] = i
+            
+            i+=1
+            
+        labels = df
+            
+        labels["%s_encoded" % column] = [encoded_labels[x] for x in df[column]]
+            
+        self.labels = encoded_labels
+        
+        return(labels)
+    
+    def trainAdaBoost(self):
+        
+        tileDataFrame = self.tilesToDataframe()
+        
+        features = np.array(tileDataFrame.drop(["class", "id"], axis=1))
+        
+        labels = self.encode_labels(tileDataFrame, "class")
+        labels = np.array(labels["class_encoded"])
+        #labels = np.array(pd.get_dummies(tileDataFrame["class"]))
+
+        train_Features, test_Features, train_Labels, test_Labels = train_test_split(
+            features, labels, test_size=0.3)
+        
+        adaboost = AdaBoostClassifier(n_estimators = 50, learning_rate=1)
+        
+        model = adaboost.fit(train_Features, train_Labels)
+        
+        self.adaBoostModel = model
+        
+        prediction = model.predict(test_Features)
+        print("Accuracy", metrics.accuracy_score(test_Labels, prediction))
+        
+    def trainRandomForest(self):
+        
+        tileDataFrame = self.tilesToDataframe()
+        
+        features = np.array(tileDataFrame.drop(["class", "id"], axis=1))
+        
+        labels = self.encode_labels(tileDataFrame, "class")
+        labels = np.array(labels["class_encoded"])
+
+        train_Features, test_Features, train_Labels, test_Labels = train_test_split(
+            features, labels, test_size=0.3)
+        
+        rfClass = RandomForestClassifier()
+        
+        model = rfClass.fit(train_Features, train_Labels)
+        
+        self.randomForestModel = model
+
+        prediction = model.predict(test_Features)
+        print("Accuracy", metrics.accuracy_score(test_Labels, prediction))
+        
+    def classify_all(self):
+        
+        shp = self.activeGeoArray.shape
+        arr = self.activeGeoArray
+        
+        l = shp[1]
+        w = shp[2]
+        
+        classified_image = np.zeros((l,w))
+        
+        for y in range(l):
+            
+            for x in range(w):
+                
+                classified_image[y, x] = self.randomForestModel.predict([arr[:,y,x]])
+        
+        print(classified_image)
+        
+        classified_image = classified_image.reshape(l, w)
+        
+        img = Image.fromarray((classified_image / np.nanmax(classified_image)) * 255)
+        img2 = img.convert("L")
+        img2.save("testBW.png")
+        
+        
+        
 
 #Extracts useful information from geo imagery, stores it, and closes the geoimage file
 #This is used to be able to save and access the relevant metadata, without clogging
@@ -610,24 +821,51 @@ class geoImageReference(object):
         
         self.bands = gimage.RasterCount
         
-        gImage = None
+        gimage = None
         
 class tile(object):
     
-    def __init__(self, data, x, y, classification, dataType):
+    def __init__(self, data, x, y, classification, dataSize):
         
-        self.data = data
+        self.data = data.reshape(dataSize, dataSize)
         
-        self.x = x
+        self.x = int(x)
         
-        self.y = y
+        self.y = int(y)
         
         self.shape = data.shape
         
         self.classification = classification
         
-        self.dataType = dataType
+        self.cleared = False
+
+
+        #Testing to ensure the data being used for the tiles is as expected
+        #print(data.reshape(10,10))
+        #test = Image.fromarray((data.reshape(10,10) / np.nanmax(data)) * 255)
+        #test = test.convert("L")
+        #test.save("test.png")
+
+    def __str__(self):
         
+        return(self.data)
+
+class spectrum(object):
+    
+    def __init__(self, data, x, y, classification, dataSize):
+        
+        self.data = data
+        
+        self.x = int(x)
+        
+        self.y = int(y)
+        
+        self.classification = classification
+        
+        self.dataSize = dataSize
+        
+        self.cleared = False
+    
     def __str__(self):
         
         return(self.data)
